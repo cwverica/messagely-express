7e5a9f4d4a46f9a02f702e07229691c26ca70276
cwverica <cwverica@gmail.com>
2021-07-10 19:04:05 -0700

    added user routes

--- routes/users.js
+++ routes/users.js
@@ -1,15 +1,39 @@
+const express = require('express');
+const router = new express.Router();
+const { User } = require('../models/user');
+const { Message } = require('../models/message');
+const ExpressError = require('../expressError')
+const { ensureCorrectUser, ensureLoggedIn } = require('../middleware/auth')
+
 /** GET / - get list of users.
  *
  * => {users: [{username, first_name, last_name, phone}, ...]}
  *
  **/
 
+router.get("/", ensureLoggedIn, async function (req, res, next) {
+    try {
+        const users = await User.all()
+        return { users };
+    } catch (e) {
+        return next(e);
+    }
+});
 
 /** GET /:username - get detail of users.
  *
  * => {user: {username, first_name, last_name, phone, join_at, last_login_at}}
  *
  **/
+
+router.get('/:username', ensureCorrectUser, async function (req, res, next) {
+    try {
+        const user = await User.get(req.user.username);
+        return { user }
+    } catch (e) {
+        return next(e);
+    }
+});
 
 
 /** GET /:username/to - get messages to user
@@ -22,6 +46,15 @@
  *
  **/
 
+router.get('/:username/to', ensureCorrectUser, async function (req, res, next) {
+    try {
+        const messages = await User.messagesTo(req.user.usernam);
+        return { messages }
+    } catch (e) {
+        return next(e);
+    }
+});
+
 
 /** GET /:username/from - get messages from user
  *
@@ -32,3 +65,12 @@
  *                 to_user: {username, first_name, last_name, phone}}, ...]}
  *
  **/
+
+router.get('/:username/from', ensureCorrectUser, async function (req, res, next) {
+    try {
+        const messages = await User.messagesFrom(req.user.usernam);
+        return { messages }
+    } catch (e) {
+        return next(e);
+    }
+});
